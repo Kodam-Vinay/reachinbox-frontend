d634aa1cfc832b73353cd137cf52e04c335528a4
Kodam-Vinay <vinay.kodam112@gmail.com>
2024-08-25 17:44:30 +0530

    completed header theme button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleTheme } from "../redux/slices/themeSlice";
+import { useSelector } from "react-redux";
 
 const Body = () => {
-  const dispatch = useDispatch();
   const isDarkMode = useSelector(
     (store) => store?.persistSliceReducer?.theme?.isDarkMode
   );
   return (
-    <>
-      <button onClick={() => dispatch(toggleTheme())}>click</button>
-      <div className="w-full h-full flex">
-        <Sidebar />
-        <div className="w-[90%] sm:w-[92%] md:w-[95%] h-full">
-          <Header />
-          <div
-            className={`${isDarkMode ? "bg-black" : "bg-[#F4F6F8]"} h-[90%]`}
-          >
-            <Outlet />
-          </div>
+    <div className="w-full h-full flex">
+      <Sidebar />
+      <div className="w-[90%] sm:w-[92%] md:w-[95%] h-full">
+        <Header />
+        <div className={`${isDarkMode ? "bg-black" : "bg-[#F4F6F8]"} h-[90%]`}>
+          <Outlet />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/ToggleThemeButton.js
+++ src/components/ToggleThemeButton.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
+import { toggleTheme } from "../redux/slices/themeSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { FiSun } from "react-icons/fi";
+import { FaMoon } from "react-icons/fa";
 
 function MyToggleButton() {
+  const dispatch = useDispatch();
   const [isChecked, setIsChecked] = useState(false);
+  const isDarkMode = useSelector(
+    (store) => store?.persistSliceReducer?.theme?.isDarkMode
+  );
+
+  useEffect(() => {
+    dispatch(toggleTheme());
+  }, [isChecked]);
 
   const handleToggle = () => {
     setIsChecked(!isChecked);
   };
-
+  console.log(isChecked);
   return (
     <div className="toggle-button">
       <input
@@ -17,9 +29,28 @@ function MyToggleButton() {
         id="toggle-checkbox"
         className="hidden"
       />
-      <label className="toggle-label m-1" htmlFor="toggle-checkbox">
-        <span className="toggle-slider"></span>
-        <span className="toggle-slider"></span>
+
+      <label
+        className="toggle-label m-1 flex items-center bg-transparent"
+        htmlFor="toggle-checkbox"
+      >
+        {!isDarkMode && (
+          <>
+            <span className="mb-2 ml-1">
+              <FaMoon color="#E8C364" />
+            </span>
+            <span className="toggle-slider ml-6"></span>
+          </>
+        )}
+
+        {isDarkMode && (
+          <>
+            <span className="toggle-slider"></span>
+            <span className="ml-7 mb-2">
+              <FiSun color="#E8C364" />
+            </span>
+          </>
+        )}
       </label>
     </div>
   );
